4d61cfc275e43bda51160bad6dbcabfa9985e0c0
agung <agung@digitalsekuriti.id>
2024-12-15 21:56:36 +0700

    fix: remove seed transaction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ItemSeeder::class,
-            TransactionSeeder::class,
         ]);
     }
 }
